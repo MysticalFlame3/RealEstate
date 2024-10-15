7c1ceb3a1aee9660d83e3ed2c7269b9aa3530fd2
MysticalFlame3 <suryaraj.9274@gmail.com>
2024-10-15 16:01:57 +0530

    Header Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import SignIn from './pages/Signin';
+import Header from './Components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes> 
     <Route path='/' element = {<Home/>}/>
     <Route path='/about' element = {<About/>}/>
-    <Route path="/sign-in" element = {<SignIn/>}/>
-    <Route path="/sign-up" element = {<SignUp/>}/>
+    <Route path="/signin" element = {<SignIn/>}/>
+    <Route path="/signup" element = {<SignUp/>}/>
     <Route path='/profile' element = {<Profile/>}/>
   </Routes>
   </BrowserRouter>
