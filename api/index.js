9274f2abb4b8201305dcbdc796269569e7b9fcb7
MysticalFlame3 <suryaraj.9274@gmail.com>
2024-10-16 13:17:48 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+console.log('Attempting to connect to MongoDB...');
+
+
+mongoose
+    .connect( "mongodb+srv://RealEstate:mongodb@realestate.spel1.mongodb.net/RealEstate?retryWrites=true&w=majority&appName=RealEstate")
+    .then(() =>{
+        console.log('Connected Successfully');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000 ...")
